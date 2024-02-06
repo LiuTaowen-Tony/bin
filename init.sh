800a3dcaf355b1e6a641a8538bcf1ab324b49d70
tony <tony_t_liu@outlook.com>
2024-02-06 17:25:49 +0000

    updated syncdotfiles

---
+++ init.sh
@@ -0,0 +1,13 @@
+# spacevim
+curl -sLf https://spacevim.org/install.sh | bash
+
+# oh-my-zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+$ZSH_CUSTOM=~/.oh-my-zsh/custom
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
