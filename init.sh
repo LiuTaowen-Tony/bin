fc4a561a38a541ec9342f6be7cf0ba9a0e67cf87
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-03-23 03:28:10 +0000

    fix omz

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ curl -sLf https://spacevim.org/install.sh | bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+mkdir ~/.oh-my-zsh
 $ZSH_CUSTOM=~/.oh-my-zsh/custom
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
